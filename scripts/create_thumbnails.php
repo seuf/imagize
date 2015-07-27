64022c0b7f7a2455650f8e6e47e0b470b126949d
Seuf <seuf76@gmail.com>
2015-07-27 21:33:49 +0200

    changed config file path

--- scripts/create_thumbnails.php
+++ scripts/create_thumbnails.php
@@ -3,7 +3,7 @@
 require_once("lib/config.php");
 require_once("lib/images.php");
 
-$config = parse_ini('config.ini');
+$config = parse_ini('config/config.ini');
 
 createThumbs($config['images_path'], $config['images_thumbs_path'], $config['thumb_size']);
 
